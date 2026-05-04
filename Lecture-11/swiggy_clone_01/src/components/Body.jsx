44d3f648a74919b75355f41ad57d1f6949474234
Samarth Vohra <samarthvohra@Samarths-MacBook-Air.local>
2026-05-04 20:42:40 +0530

    swiggy clone day of react

--- Lecture-11/swiggy_clone_01/src/components/Body.jsx
+++ Lecture-11/swiggy_clone_01/src/components/Body.jsx
@@ -1,22 +1,20 @@
 import React from 'react'
 import RestaurantCard from './RestaurantCard'
+import ApiCalling from './ApiCalling'
 
 function Body() {
+
+    let restArray = ApiCalling()
+
   return (
     <div>
         <h1 className='font-bold text-2xl m-10'>Restaurants with online food delivery in Chhindwara</h1>
     
-        <button className='border rounded w-1/12 m-2 ml-12 p-2'>Rating 4.4+</button>
-        <button className='border rounded w-1/12 ml-20 p-2'>Reset</button>
+        <button  className='border rounded w-1/12 m-2 ml-12 p-2'>Rating 4.3+</button>
+        <button  className='border rounded w-1/12 ml-20 p-2'>Reset</button>
 
         <div className='flex flex-wrap w-10/12 m-auto'>
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
-            <RestaurantCard />
+           <RestaurantCard restArray={restArray} />
         </div>
     </div>
   )
