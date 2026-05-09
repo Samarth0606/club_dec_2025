a0e018d1359585c13eb6c12f799c0224d10721e4
Samarth Vohra <samarthvohra@Samarths-MacBook-Air.local>
2026-05-09 12:06:46 +0530

    13th day of react

---
+++ Lecture-13/swiggy_clone_03/src/components/Help.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+function Help() {
+  return (
+    <div>
+        {/* <Header />
+        <h1>help</h1>
+        <Footer /> */}
+        <h1>help</h1>
+    </div>
+  )
+}
+
+export default Help
---
+++ Lecture-13/swiggy_clone_03/src/main.jsx
@@ -0,0 +1,48 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Offer from './components/Offer.jsx'
+import Help from './components/Help.jsx'
+import Signin from './components/Signin.jsx'
+import Body from './components/Body.jsx'
+import Cart from './components/Cart.jsx'
+import RestaurantDetails from './components/RestaurantDetails.jsx'
+
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element: <App />,
+    children:[
+      {
+        path:"/",
+        element: <Body />
+      },
+      {
+        path:"/offer",
+        element: <Offer />
+      },
+      {
+        path:"/help",
+        element: <Help />
+      },
+      {
+        path:"/signin",
+        element: <Signin />
+      },
+      {
+        path:"/cart",
+        element: <Cart />
+      },
+      {
+        path:"/restaurant/:idd",
+        element: <RestaurantDetails />
+      }
+
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={appRouter}></RouterProvider>
+)
