88f800b42cfb8a15ff013b6759e280f9e75be899
Samarth Vohra <samarthvohra@Samarths-MacBook-Air.local>
2026-06-08 20:57:46 +0530

    typescript

--- Lecture-19/swiggy_03/index.js
+++ Lecture-19/swiggy_03/index.js
@@ -3,11 +3,16 @@ const app = express();
 const mongoose = require('mongoose');
 const restaurantRoutes = require('./routes/restaurant.routes')
 const userRoutes = require('./routes/user.routes')
+const cors = require('cors')
+
+
+// Adds headers: Access-Control-Allow-Origin: *
 
 mongoose.connect('mongodb+srv://samarthvohraindia_db_user:L63bjsfKotl7tdmA@cluster0.ooa8pji.mongodb.net/?appName=Cluster0')
 .then(()=>console.log(`DB IS CONNECTED`) )
 .catch(()=>console.log(`DB IS NOT CONNECTED`) )
 
+app.use(cors()) //cors
 app.use(express.json()); //body parsing middleware
 
 app.get('/' , (req,res)=>{
---
+++ Lecture-20/dist/a.js
@@ -0,0 +1,55 @@
+"use strict";
+// let a:number = 10; //automatically taking it => inference (interview)
+// console.log(a);
+// // a = "sam";
+// a = 2394;
+// console.log(a);
+// -------------------------
+// function greet(name:string){
+//     console.log(`hello from ${name}`);
+// }
+// greet("sam")
+// -------------------------
+// function greet(a:number , b:number):number{
+//    return a+b
+// }
+// greet(12 , 34)
+// -------------------------
+// function age(a:number):boolean{
+//     if(a>18){
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+// let out:boolean = age(19)
+// console.log(out);
+// -------------------------
+// function runAfter1Sec(fn:()=>void):number{
+//     setTimeout(()=>{
+//         fn();
+//     } , 1000)
+//     return 10;
+// }
+// function called(){
+//     console.log("hey there...");
+// }
+// let out:number = runAfter1Sec(called)
+// -------------------------
+// function runAfter1Sec(fn:()=>number):number{
+//     setTimeout(()=>{
+//         fn();
+//     } , 1000)
+//     return 10;
+// }
+// function called(){
+//     console.log("hey there...");
+//     return 500;
+// }
+// let out:number = runAfter1Sec(called)
+// -------------------------
+var sam = function () {
+    console.log("hello");
+};
+sam();
+// -------------------------
